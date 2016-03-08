50219601607826e5785a63bb618a90b09d353396
testfile2 <testcom132@gmail.com>
2016-03-08 15:39:50 +0200

    Update ft_strstr.c

--- ex02/ft_strstr.c
+++ ex02/ft_strstr.c
@@ -10,41 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-char	*ft_strstr(char *str, char *to_find)
+char    *ft_strstr(char *str, char *to_find)
 {
-	char ref;
-	int index;
-	int find;
+    int index;
+    int find;
 
-	index = 0;
-	find = 0;
-	while (find <= index && str[index] != '\0')
-	{
-		if (str[index] == to_find[find] && str[index] != '\0')
-		{
-				ref = to_find[find];
-				write (1, &ref, 1);
-				index++;
-				find++;
-		}
-		else
-		{
-			while (str[index] != to_find[find])
-			{
-				index++;
-			}
-		}
-	}
-	return 0;
-}
-
-int main()
-{
-	char str1[] = "12962344234";
-	char str2[] = "96";
-
-	ft_strstr(str1, str2);
-	return 0;
+    index = 0;
+    while (str[index] != '\0')
+    {
+        find = 0;
+        while (str[index + i] == to_find[i])
+        {
+            i++;
+            if (to_find[i] == '\0')
+            {
+                return (to_find);
+            }
+        }
+        index++;
+    }
+    return 0;
 }
